cb914ef7919d549598df42f3b646b096a015200a
MatanR <matan@salaryo.com>
2020-09-20 10:52:33 +0300

    create new user

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,12 +1,20 @@
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import {CognitoUser, CognitoUserAttribute, CognitoUserPool} from 'amazon-cognito-identity-js'
 import { User } from './user.model';
+
+const POOL_DATA = {
+    UserPoolId: ' us-east-2_LLdTd8MKQ',
+    ClientId: 'nrn6b4a027tcobk82pgdr995e'
+}
+const userPool = new CognitoUserPool(POOL_DATA)
 
 export default class AuthService {
     public static authIsLoading = new BehaviorSubject<boolean>(false);
     public static authDidFail = new BehaviorSubject<boolean>(false);
     public static authStatusChanged = new Subject<boolean>();
+    public static registeredUser: CognitoUser;
 
     public static signUp(username: string, email: string, password: string): void {
         this.authIsLoading.next(true);
@@ -15,10 +23,28 @@ export default class AuthService {
             email: email,
             password: password
         };
+        const attributeList: CognitoUserAttribute[] = [];
+
         const emailAttribute = {
-            Name: 'email',
+            Name: 'email', // the name of the attribute on cognito
             Value: user.email
         };
+        attributeList.push(new CognitoUserAttribute(emailAttribute));
+
+        userPool.signUp(user.username, user.password, attributeList, [], (err, result) => {
+            if(err) {
+                this.authDidFail.next(true);
+                this.authIsLoading.next(false);
+                return;
+            }else {
+                this.authDidFail.next(false);
+                this.authIsLoading.next(false);
+                if(result?.user) {
+                    this.registeredUser = result?.user;
+                }
+                console.log(result)
+            }
+        });
         return;
     }
     public static confirmUser(username: string, code: string) {
